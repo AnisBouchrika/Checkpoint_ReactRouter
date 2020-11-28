566216a32bb94a874740496589ac7c6aaffe69ee
AnisBouchrika <anice.bouchrika@gmail.com>
2020-11-28 16:58:49 +0100

    movies-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Search from "./components/Search/Search";
+import MovieList from "./components/MovieList/MovieList";
+import { moviesData  } from "./Data";
+import AddMovie from './components/AddMovie/AddMovie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+const App = () => {
+  const [movies , setMovies] = useState(moviesData);
+  const [searchRate, setSearchRate] = useState(1);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = (event) => setSearchValue(event.target.value);
+  const handleSearchRate = (newRate) => setSearchRate(newRate);
+
+
+
+
+ const addMovie = (newMovie) => setMovies([...movies, newMovie]);
+  return(
+      <div>
+          <Search
+        searchValue={searchValue}
+        handleSearch={handleSearch}
+        searchRate={searchRate}
+        handleSearchRate={handleSearchRate}
+      />
+       <MovieList
+        movieList={movies.filter(
+          (movie) =>
+            movie.name
+              .toLowerCase()
+              .includes(searchValue.toLowerCase().trim()) &&
+            movie.rating >= searchRate
+        )}
+      />
+         <AddMovie handleAdd = {addMovie} />
+
+      </div>
+  )
+};
+
+export default App
+
